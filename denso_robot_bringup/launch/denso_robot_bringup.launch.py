e217b3f29a352602a78e67749eec64317e638271
pedrocgr <pedroc.gr@hotmail.com>
2026-04-09 15:54:24 -0300

    fix default calib_xyz and calib_mesh_xyz positions in launch file

--- denso_robot_bringup/launch/denso_robot_bringup.launch.py
+++ denso_robot_bringup/launch/denso_robot_bringup.launch.py
@@ -169,7 +169,7 @@ def generate_launch_description():
         ))
     declared_arguments.append(
         DeclareLaunchArgument(
-            'calib_xyz', default_value='0 0 0.01',
+            'calib_xyz', default_value='0 0 0.0475',
             description='XYZ position of calib tool relative to J6'
         ))
     declared_arguments.append(
@@ -179,7 +179,7 @@ def generate_launch_description():
         ))
     declared_arguments.append(
         DeclareLaunchArgument(
-            'calib_mesh_xyz', default_value='0 0 0',
+            'calib_mesh_xyz', default_value='0 0 -0.039',
             description='XYZ offset of calib tool mesh to compensate for Fusion export'
         ))
     declared_arguments.append(
